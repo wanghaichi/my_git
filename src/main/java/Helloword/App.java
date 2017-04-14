d9a5212d30c71fa418d9e0fc0f3b51a57166499e
wanghaichi <703007993@qq.com>
2017-04-14 15:02:49 +0800

    update

--- src/main/java/Helloword/App.java
+++ src/main/java/Helloword/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Wang Haichi" );
+        System.out.println( "Hello Wang Hanchao" );
+
     }
 }
